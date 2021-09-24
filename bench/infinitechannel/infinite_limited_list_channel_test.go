e1724c3da03805d53bde7058cda158787a60cca2
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2021-09-24 11:14:57 +0300

    Infinite channel backed by limited priority queue slice implementation benchmarks

--- bench/infinitechannel/infinite_limited_list_channel_test.go
+++ bench/infinitechannel/infinite_limited_list_channel_test.go
@@ -24,22 +24,10 @@ func BenchmarkInfiniteLimitLimitedPriorityListChannelNoLimitTransfer10kSerial(b 
 	}, b)
 }
 
-func benchmarkInfiniteLimitLimitedPriorityChannelNoLimitTransfer10kSerial(makeLimitedChannelFun func(limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkDefaultInfiniteChannelTransfer10kSerial(func() *infinitechannel.InfiniteChannel {
-		return makeLimitedChannelFun(12000)
-	}, b)
-}
-
 func BenchmarkInfiniteLimitLimitedPriorityListChannelTransfer10kSerial(b *testing.B) {
 	benchmarkInfiniteLimitLimitedPriorityChannelTransfer10kSerial(func(limit int) *infinitechannel.InfiniteChannel {
 		return infinitechannel.NewInfiniteLimitLimitedPriorityListChannel(limit)
 	}, b)
-}
-
-func benchmarkInfiniteLimitLimitedPriorityChannelTransfer10kSerial(makeLimitedChannelFun func(limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkInfiniteChannelTransfer10kSerial(func() *infinitechannel.InfiniteChannel {
-		return makeLimitedChannelFun(10000)
-	}, 12000, b)
 }
 
 func BenchmarkInfiniteLimitedPriorityListChannelNoLimitTransfer10kSerial(b *testing.B) {
@@ -48,25 +36,10 @@ func BenchmarkInfiniteLimitedPriorityListChannelNoLimitTransfer10kSerial(b *test
 	}, b)
 }
 
-func benchmarkInfiniteLimitedPriorityChannelNoLimitTransfer10kSerial(makeLimitedPriorityChannelFun func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkInfinitePriorityChannelTransfer10kSerial(func(fun func(i interface{}) bool) *infinitechannel.InfiniteChannel {
-		return makeLimitedPriorityChannelFun(fun, 12000)
-	}, b)
-}
-
 func BenchmarkInfiniteLimitedPriorityListChannelTransfer10kSerial(b *testing.B) {
 	benchmarkInfiniteLimitedPriorityChannelTransfer10kSerial(func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel {
 		return infinitechannel.NewInfiniteLimitedPriorityListChannel(fun, limit)
 	}, b)
-}
-
-func benchmarkInfiniteLimitedPriorityChannelTransfer10kSerial(makeLimitedPriorityChannelFun func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	makeInfiniteChannelFun := func() *infinitechannel.InfiniteChannel {
-		return makeLimitedPriorityChannelFun(func(i interface{}) bool {
-			return i.(int)%2 == 0
-		}, 10000)
-	}
-	benchmarkInfiniteChannelTransfer10kSerial(makeInfiniteChannelFun, 12000, b)
 }
 
 //--
@@ -89,22 +62,10 @@ func BenchmarkInfiniteLimitLimitedPriorityListChannelNoLimitTransfer10kConcurren
 	}, b)
 }
 
-func benchmarkInfiniteLimitLimitedPriorityChannelNoLimitTransfer10kConcurrent(makeLimitedChannelFun func(limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkDefaultInfiniteChannelTransfer10kConcurrent(func() *infinitechannel.InfiniteChannel {
-		return makeLimitedChannelFun(12000)
-	}, b)
-}
-
 func BenchmarkInfiniteLimitLimitedPriorityListChannelTransfer10kConcurrent(b *testing.B) {
 	benchmarkInfiniteLimitLimitedPriorityChannelTransfer10kConcurrent(func(limit int) *infinitechannel.InfiniteChannel {
 		return infinitechannel.NewInfiniteLimitLimitedPriorityListChannel(limit)
 	}, b)
-}
-
-func benchmarkInfiniteLimitLimitedPriorityChannelTransfer10kConcurrent(makeLimitedChannelFun func(limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkInfiniteChannelTransfer10kConcurrent(func() *infinitechannel.InfiniteChannel {
-		return makeLimitedChannelFun(10000)
-	}, 12000, b)
 }
 
 func BenchmarkInfiniteLimitedPriorityListChannelNoLimitTransfer10kConcurrent(b *testing.B) {
@@ -113,23 +74,8 @@ func BenchmarkInfiniteLimitedPriorityListChannelNoLimitTransfer10kConcurrent(b *
 	}, b)
 }
 
-func benchmarkInfiniteLimitedPriorityChannelNoLimitTransfer10kConcurrent(makeLimitedPriorityChannelFun func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	benchmarkInfinitePriorityChannelTransfer10kConcurrent(func(fun func(i interface{}) bool) *infinitechannel.InfiniteChannel {
-		return makeLimitedPriorityChannelFun(fun, 12000)
-	}, b)
-}
-
 func BenchmarkInfiniteLimitedPriorityListChannelTransfer10kConcurrent(b *testing.B) {
 	benchmarkInfiniteLimitedPriorityChannelTransfer10kConcurrent(func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel {
 		return infinitechannel.NewInfiniteLimitedPriorityListChannel(fun, limit)
 	}, b)
-}
-
-func benchmarkInfiniteLimitedPriorityChannelTransfer10kConcurrent(makeLimitedPriorityChannelFun func(fun func(interface{}) bool, limit int) *infinitechannel.InfiniteChannel, b *testing.B) {
-	makeInfiniteChannelFun := func() *infinitechannel.InfiniteChannel {
-		return makeLimitedPriorityChannelFun(func(i interface{}) bool {
-			return i.(int)%2 == 0
-		}, 10000)
-	}
-	benchmarkInfiniteChannelTransfer10kConcurrent(makeInfiniteChannelFun, 12000, b)
 }
